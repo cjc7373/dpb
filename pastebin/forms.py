86ba59deab17f3dd19b1cd5eae34fb9924ec2f90
cjc7373 <niuchangcun@gmail.com>
2022-02-12 15:57:49 +0800

    styles tweak

--- pastebin/forms.py
+++ pastebin/forms.py
@@ -2,4 +2,7 @@ from django import forms
 
 
 class PastebinForm(forms.Form):
-    content = forms.CharField(max_length=1024 * 512, widget=forms.Textarea)
+    content = forms.CharField(
+        max_length=1024 * 512,
+        widget=forms.Textarea(attrs={'style': 'width: 100%; ', 'cols': ''}),
+    )
